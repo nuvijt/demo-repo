3d340068549278fa829f95bfcfd40a9615eb22c6
simba <miwenger@outlook.com>
2024-11-22 17:52:04 +0100

    Lektion 2 DTC Testen von Tastenkombinationen!

---
+++ src/Lektion2.kt
@@ -0,0 +1,15 @@
+fun getUserName(): String {
+    return "Simba"
+}
+
+fun greetings(): String {
+    return "Hello, ${getUserName()}"
+}
+
+fun main() {
+        println("Hi ${getUserName()}")
+        println(greetings())
+}
+fun timeout() {
+    timeout()
+}
